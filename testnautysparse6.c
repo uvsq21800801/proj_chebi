c3a91c4c18058d31f8d52e5d31c90ba487a2f663
Djillali Boutouili <38718985+Djellool@users.noreply.github.com>
2023-01-18 22:15:31 +0100

    Add files via upload
    Contient des fonctions qui à partir de deux ID de molecules dans CHEBI arrivent à dire si ces molecules sont iso ou pas.
    ps: les molecules sont stockés dans un format Sparse6

---
+++ testnautysparse6.c
@@ -0,0 +1,105 @@
+/* This program demonstrates how an isomorphism is found between
+   two graphs, using the Moebius graph as an example.
+   This version uses sparse form with dynamic allocation.
+*/
+
+#include "nausparse.h"    /* which includes nauty.h */
+#include <ctype.h>
+
+
+// lis un fichier texte generér par gtools et le transforme en sparse graphe 
+sparsegraph lire_sparse_graphe_fichier(int id_molecule) {
+	
+	char* nom_fichier;
+	nom_fichier = (char*) malloc(sizeof(char) * 100);
+	strcpy(nom_fichier, "sparse6mol/");
+	sprintf(&nom_fichier[strlen("sparse6mol/")], "%d", id_molecule);
+    strcat(nom_fichier, ".txt");
+
+
+	FILE * fp;
+
+   	fp = fopen (nom_fichier, "rb+");
+    char buffer[1000];
+    sparsegraph sg;
+	while ( fscanf(fp, "%s", buffer) != EOF )
+		{   
+		   SG_INIT(sg);
+		   int num_loops;
+		   stringtosparsegraph(buffer, &sg, &num_loops);
+		}
+	fclose(fp);
+	return sg;
+}
+
+void out_sparseGraphe(char* nom_fichier, sparsegraph sg1){
+	/* On affiche les graphes dans un format lisible par l'humain */
+	
+        FILE * fp;
+   		fp = fopen (nom_fichier, "w+");
+        put_sg(fp,&sg1,1,0);
+        fclose(fp);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+    DYNALLSTAT(int,lab1,lab1_sz);
+    DYNALLSTAT(int,lab2,lab2_sz);
+    DYNALLSTAT(int,ptn,ptn_sz);
+    DYNALLSTAT(int,orbits,orbits_sz);
+    DYNALLSTAT(int,map,map_sz);
+    static DEFAULTOPTIONS_SPARSEGRAPH(options);
+    statsblk stats;
+    
+    int id_molecule1;
+    int id_molecule2;
+    
+ /* Declare and initialize sparse graph structures */
+    SG_DECL(cg1); SG_DECL(cg2);
+
+    int n,m,i;
+
+ /* Select option for canonical labelling */
+
+    options.getcanon = TRUE;
+    
+ 
+
+	n=100;
+	m = SETWORDSNEEDED(n);
+    nauty_check(WORDSIZE,m,n,NAUTYVERSIONID);
+
+    DYNALLOC1(int,lab1,lab1_sz,n,"malloc");
+    DYNALLOC1(int,lab2,lab2_sz,n,"malloc");
+    DYNALLOC1(int,ptn,ptn_sz,n,"malloc");
+    DYNALLOC1(int,orbits,orbits_sz,n,"malloc");
+    DYNALLOC1(int,map,map_sz,n,"malloc");
+
+    
+    printf("Veuillez introduire l'id de la premiére molécule : ");
+    scanf("%d",&id_molecule1);
+    printf("\nVeuillez maintenant introduire l'i'd de la deuxiéme molécule : ");
+    scanf("%d",&id_molecule2);
+    sparsegraph sg1 = lire_sparse_graphe_fichier(id_molecule1);
+    sparsegraph sg2 = lire_sparse_graphe_fichier(id_molecule2);
+    
+    /*on applique sparse nauty sur les deux graphes */
+            
+    sparsenauty(&sg1,lab1,ptn,orbits,&options,&stats,&cg1);
+    sparsenauty(&sg2,lab2,ptn,orbits,&options,&stats,&cg2);
+            
+    
+    if (aresame_sg(&cg1,&cg2))
+        {
+            printf("\nLes graphes sont isomoprhes ! \n");
+        }
+        else{
+            printf("\nLes graphes ne sont pas isomorphes.\n");
+        }
+	
+	    
+    printf("Press ENTER key to Continue\n");  
+	getchar();
+}
