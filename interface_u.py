9c5b2192e62ec6962e1cdb37c83b1403f31187db
uvsq21800801 <fugu.swag@gmail.com>
2023-01-21 13:21:18 +0100

    plan pour faire l'assemblage

--- interface_u.py
+++ interface_u.py
@@ -1,5 +1,19 @@
 import ctypes
 
 
+def interface():
+    print('interface avec appel de nauty TODO')
 
-print('interface avec appel de nauty TODO')
+    # demande deux id/noms aux utilisateurs avec boucles de faux dans le cas
+    # ou ça n'est pas trouvé dans la bdd
+
+    # possiblement une interface plus tards
+
+    # appelle nauty avec les deux ids et vérifie l'isomorphisme
+
+
+    # il serait aussi bien que l'affichage des deux entités
+    # appellées soient faites quelquepart
+
+
+interface()
